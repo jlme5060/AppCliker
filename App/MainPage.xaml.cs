69ec032445ab97dfe452ad367b17582970b6fa0f
jose.martinez30 <jose.martinez30@5300101A107029>
2025-01-23 16:36:20 +0100

    Agregar archivos de proyecto.

---
+++ App/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿
+namespace App
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count <= 9)
+                CounterBtn.Text = $"has hecho  {count} golpes";
+            else
+            {
+                MonsterImage.Source = "tumba.jpg";
+                CounterBtn.Text = $"amenazada eliminada";
+            }
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+}
